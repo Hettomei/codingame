bba8eda84fd929bc4b0c02fb3845fa60fc5840e1
Hettomei <itsumo.sibyllin@gmail.com>
2016-08-14 20:37:07 +0200

    mime: add more test suite, because it fail

--- clj-mimetype/src/clj_mimetype/core.clj
+++ clj-mimetype/src/clj_mimetype/core.clj
@@ -3,25 +3,30 @@
   (:require [clojure.string :as str]))
 
 (defn array-to-low [array]
-  (map (fn [e](str/lower-case e)) array))
+  (map #(str/lower-case %) array))
 
 (defn array-to-split [array]
-  (map (fn [f](str/split f #"\.")) array))
+  (map #(str/split % #"\.") array))
 
 (defn array-to-ext-hash [array]
   (let [map-ext (map #(str/split % #" ") array)
-        down (map (fn [a] [(str/lower-case (first a)) (last a)]) map-ext)
-        ext-to-mime (into {} down)]
-    ext-to-mime))
+        down (map (fn [a] [(str/lower-case (first a)) (last a)]) map-ext)]
+    (into {} down)))
 
 (defn -main
   [& args]
-  (let [n-extension (read) n-file (read) _ (read-line)]
-    (let [extensions (repeatedly n-extension read-line)
-          ext-to-mime (array-to-ext-hash extensions) ]
-      (let [files (repeatedly n-file read-line)
-            files-low (array-to-low files)
-            files-ext (array-to-split files-low)
-            only-ext (map #(second %) files-ext)]
-        (println (str/join "\n" (map #(ext-to-mime % "UNKNOWN") only-ext)))
-        ))))
+  (let [n-extension (read) n-file (read) _ (read-line)
+        extensions (repeatedly n-extension read-line)
+        ext-to-mime (array-to-ext-hash extensions) ]
+    (let [files (repeatedly n-file read-line)
+          files-low (array-to-low files)
+          files-replace (map (fn [a] (str/replace a #"\.$" "-")) files-low)
+          files-ext (array-to-split files-replace)
+          only-ext (map #(last (rest %)) files-ext)]
+      (prn files-low)
+      (prn files-replace)
+      (prn files-ext)
+      (prn only-ext)
+      (println "---------------------------------------------------")
+      (println (str/join "\n" (map #(ext-to-mime % "UNKNOWN") only-ext)))
+      )))
