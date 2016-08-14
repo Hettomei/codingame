990964af9863893b56df545b10037f1082294b34
Hettomei <itsumo.sibyllin@gmail.com>
2016-08-14 19:52:55 +0200

    mime: it works !

--- clj-mimetype/src/clj_mimetype/core.clj
+++ clj-mimetype/src/clj_mimetype/core.clj
@@ -11,26 +11,19 @@
 (defn -main
   [& args]
   (println "          ")
-  (println "          ")
   (let [n-extension (read) n-file (read) _ (read-line)]
     (println n-extension n-file)
     (let [extensions (repeatedly n-extension read-line)
           map-ext (map #(str/split % #" ") extensions)
           down (map (fn [a] [(str/lower-case (first a)) (last a)]) map-ext)
+          ext-to-mime (into {} down)
           ]
-      (prn extensions)
-      (prn map-ext)
-      (prn down)
+      (prn ext-to-mime)
       (println "-------------")
       (let [files (repeatedly n-file read-line)
             files-low (array-to-low files)
             files-ext (array-to-split files-low)
-            only-ext (map #(second %) files-ext)
-            ]
-        (prn files)
-        (prn files-low)
-        (prn files-ext)
+            only-ext (map #(second %) files-ext)]
         (prn only-ext)
-        )
-      ))
-  (println 'fin))
+        (println (str/join "\n" (map #(ext-to-mime % "UNKNOWN") only-ext)))
+        ))))
