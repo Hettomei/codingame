62c8cf2f54b961ffe9b382114ca9a89c07d06a34
Hettomei <itsumo.sibyllin@gmail.com>
2016-08-14 19:02:20 +0200

    mime: add str lib

--- clj-mimetype/src/clj_mimetype/core.clj
+++ clj-mimetype/src/clj_mimetype/core.clj
@@ -1,5 +1,7 @@
 (ns clj-mimetype.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string :as str]))
+
 
 (defn -main
   [& args]
@@ -8,16 +10,16 @@
   (let [n-extension (read) n-file (read) _ (read-line)]
     (println n-extension n-file)
     (let [extensions (repeatedly n-extension read-line)
-          map-ext (map #(clojure.string/split % #" ") extensions)
-          down (map (fn [a] [(clojure.string/lower-case (first a)) (last a)]) map-ext)
+          map-ext (map #(str/split % #" ") extensions)
+          down (map (fn [a] [(str/lower-case (first a)) (last a)]) map-ext)
           ]
       (prn extensions)
       (prn map-ext)
       (prn down)
       (println "-------------")
       (let [files (repeatedly n-file read-line)
-            files-low (map (fn [f](clojure.string/lower-case f)) files)
-            files-ext (map (fn [f](clojure.string/split f #".")) files-low)
+            files-low (map (fn [f](str/lower-case f)) files)
+            files-ext (map (fn [f](str/split f #"\.")) files-low)
             ]
         (prn files)
         (prn files-low)
