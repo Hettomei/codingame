822e382b3a59e1d3a74c725e1bdb7b7e803d8656
Hettomei <itsumo.sibyllin@gmail.com>
2017-08-19 23:27:29 +0200

    clj panic works

--- clj-panic/src/clj_panic/core.clj
+++ clj-panic/src/clj_panic/core.clj
@@ -7,35 +7,36 @@
 ; Auto-generated code below aims at helping you parse
 ; the standard input according to the problem statement.
 
+(defn create-exits [exitFloor exitPos elevators]
+  (let [map-exit {:floor exitFloor :pos exitPos}
+        map-elevators (map #(zipmap '(:floor :pos) %) (partition 2 elevators))]
+    (set (list* map-exit map-elevators))))
+
+(defn find-exit [floor coll]
+  (first (filter #(= (:floor %) floor) coll)))
+
+; action: WAIT or BLOCK
+(defn action [direction next-exit clonePos]
+  (if (or
+        (and (= direction 'RIGHT) (>= (:pos next-exit) clonePos))
+        (and (= direction 'LEFT) (<= (:pos next-exit) clonePos)))
+    'WAIT
+    'BLOCK))
+
 (defn -main [& args]
   (let [nbFloors (read) ; unused
         width (read) ; unused
-        nbRounds (read)
+        nbRounds (read) ; unused
         exitFloor (read)
         exitPos (read)
-        nbTotalClones (read)
-        nbAdditionalElevators (read)
-        nbElevators (read)]
-    ; nbFloors: number of floors
-    ; width: width of the area
-    ; nbRounds: maximum number of rounds
-    ; exitFloor: floor on which the exit is found
-    ; exitPos: position of the exit on its floor
-    ; nbTotalClones: number of generated clones
-    ; nbAdditionalElevators: ignore (always zero)
-    ; nbElevators: number of elevators
-    (loop [i nbElevators]
-      (when (> i 0)
-        (let [elevatorFloor (read) elevatorPos (read)]
-          ; elevatorFloor: floor on which this elevator is found
-          ; elevatorPos: position of the elevator on its floor
-          (recur (dec i)))))
+        nbTotalClones (read) ; unused
+        nbAdditionalElevators (read) ; unused
+        nbElevators (read)
+        ; nbElevators lignes suivantes : un couple d'entiers elevatorFloor elevatorPos donnant respectivement l'étage et la position d'un ascenseur.
+        exits (create-exits exitFloor exitPos (repeatedly (* 2 nbElevators) read))]
     (while true
-      (let [cloneFloor (read) clonePos (read) direction (read)]
-        ; cloneFloor: floor of the leading clone
-        ; clonePos: position of the leading clone on its floor
-        ; direction: direction of the leading clone: LEFT or RIGHT
-
-
-        ; action: WAIT or BLOCK
-        (println "WAIT")))))
+      (let [cloneFloor (read) ; floor of the leading clone
+            clonePos (read) ; position of the leading clone on its floor
+            direction (read) ; direction of the leading clone: LEFT or RIGHT
+            next-exit (find-exit cloneFloor exits)]
+        (println (action direction next-exit clonePos))))))
--- clj-panic/test/clj_panic/core_test.clj
+++ clj-panic/test/clj_panic/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [clj-panic.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
+(deftest test-create-exits
+  (testing "It create a set of map"
+    (is (= #{{:floor 5 :pos 19}} (create-exits 5 19 '())))
+
+    (is (= #{{:floor 3 :pos 22} {:floor 5 :pos 19}} (create-exits 5 19 '(3 22))))
+
+    (is (= #{{:floor 0 :pos 17}
+             {:floor 1 :pos 12}
+             {:floor 2 :pos 10}
+             {:floor 5 :pos 19}} (create-exits 5 19 '(2 10 0 17 1 12))))
+    ))
+
+(deftest test-find-exit
+  (testing "return a map"
+    (is (= nil (find-exit 5 #{{:floor 3 :pos 2}})))
+    (is (= {:floor 5 :pos 2} (find-exit 5 #{{:floor 3 :pos 2} {:floor 5 :pos 2}})))
+    ))
+
+(deftest test-action
+  (testing "action when wait"
+    (is (= 'WAIT (action 'RIGHT {:floor 4 :pos 12} 12)))
+    (is (= 'WAIT (action 'RIGHT {:floor 4 :pos 12} 11)))
+    (is (= 'WAIT (action 'LEFT {:floor 4 :pos 4} 4)))
+    (is (= 'WAIT (action 'LEFT {:floor 4 :pos 4} 5))))
+  (testing "action when Block"
+    (is (= 'BLOCK (action 'RIGHT {:floor 4 :pos 12} 13)))
+    (is (= 'BLOCK (action 'LEFT {:floor 4 :pos 4} 3)))))
