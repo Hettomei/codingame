1a24f4a15ebfbd9a1bb29c7cb8bf6b06e383edd3
Hettomei <itsumo.sibyllin@gmail.com>
2017-09-17 22:58:34 +0200

    labyrinthe

--- clj-the-labyrinth/src/clj_the_labyrinth/core.clj
+++ clj-the-labyrinth/src/clj_the_labyrinth/core.clj
@@ -1,19 +1,30 @@
 (ns clj-the-labyrinth.core
   (:gen-class))
 
+; (ns Player
+;   (:gen-class))
+
 (defn previous-movement [previous]
   (get {nil 'RIGHT
         'RIGHT 'RIGHT
         'UP 'RIGHT
         'LEFT 'UP
-        'BOTTOM 'LEFT} previous)
-  )
+        'DOWN 'LEFT} previous))
 
 (defn after-movement [mov]
   (get {'RIGHT 'UP
         'UP 'LEFT
-        'LEFT 'BOTTOM} mov)
-  )
+        'LEFT 'DOWN
+        'DOWN 'FAIL} mov))
+
+(defn add-position [position mov]
+  (let [add (get {'UP     '(-1  0)
+                  'DOWN '( 1  0)
+                  'LEFT   '( 0 -1)
+                  'RIGHT  '( 0  1)} mov)
+        x (first position)
+        y (last  position)]
+    (list (+ x (first add)) (+ y (last add)))))
 
 (defn indices-of [f coll]
   (keep-indexed #(if (f %2) %1 nil) coll))
@@ -21,29 +32,30 @@
 (defn pos-in-str [line a-char]
   (first (indices-of (partial = a-char) line)))
 
-(defn find-2d-string [a-char lab-2d]
-  (reduce
-    (fn [idx line]
-      (let [pos (pos-in-str line a-char)]
-        (if pos
-          (reduced (list idx pos))
-          (inc idx))
-        ))
-    0
-    lab-2d))
+(defn get-place-in-lab [lab x-y]
+  (nth (nth lab (first x-y) '()) (second x-y) nil))
 
-(defn can-go? [lab mov]
-  ; find T
-  ; then find next point (up down ...)
-  ; return next point is a '.'
-  (let [T (find-2d-string \T lab)]
-    T))
+(defn next-place [lab mov pos]
+  (get-place-in-lab lab (add-position pos mov)))
 
-(defn next-move [labyrinth move-to]
-  (if (can-go? labyrinth move-to)
-    move-to
-    (next-move labyrinth (after-movement move-to))
-    ))
+(defn next-move [labyrinth move-to pos]
+  (cond
+    (= 'FAIL move-to) move-to
+    (= \. (next-place labyrinth move-to pos)) move-to
+    :else (next-move labyrinth (after-movement move-to) pos)))
+
+
+(defn start [R last-m]
+  (let [pos (list (read) (read)) _ (read-line)
+        ROWS (repeatedly R read-line)
+        out (next-move ROWS (previous-movement last-m) pos)]
+      (binding [*out* *err*]
+         (println (clojure.string/join "\n" ROWS))
+         (println pos)
+         )
+   (println out)
+   (start R out)))
 
 (defn -main [& args]
-  (next-move '(("####..T..#")) (previous-movement nil)))
+  (let [R (read) __ (read-line)]
+    (start R nil)))
--- clj-the-labyrinth/test/clj_the_labyrinth/core_test.clj
+++ clj-the-labyrinth/test/clj_the_labyrinth/core_test.clj
@@ -6,32 +6,53 @@
   (is (= 'RIGHT (previous-movement nil)))
   (is (= 'RIGHT (previous-movement 'RIGHT)))
   (is (= 'RIGHT (previous-movement 'UP)))
-  (is (= 'UP (previous-movement 'LEFT)))
-  (is (= 'LEFT (previous-movement 'BOTTOM))))
+  (is (= 'UP    (previous-movement 'LEFT)))
+  (is (= 'LEFT  (previous-movement 'BOTTOM))))
 
 (deftest test-after-movement
-  (is (= 'UP (after-movement 'RIGHT)))
-  (is (= 'LEFT (after-movement 'UP)))
-  (is (= 'BOTTOM    (after-movement 'LEFT)))
-  (is (= nil  (after-movement 'BOTTOM))))
+  (is (= 'UP     (after-movement 'RIGHT)))
+  (is (= 'LEFT   (after-movement 'UP)))
+  (is (= 'BOTTOM (after-movement 'LEFT)))
+  (is (= 'FAIL   (after-movement 'BOTTOM))))
+
+(deftest test-add-position
+  (is (= '(4 6)     (add-position '(4 5) 'RIGHT)))
+  (is (= '(1 0)   (add-position '(0 0) 'BOTTOM))))
 
 (deftest test-find-2d-string
   (is (= '(0 0) (find-2d-string \T '("T."))))
   (is (= '(1 2) (find-2d-string \T '("...." "..T." "....")))))
 
-(deftest test-can-go?
-  (testing "go right"
-    (let [input '("...T.")]
-      (is (= true (can-go? input 'RIGHT)))
-      )
-    )
-  )
+(deftest test-next-place
+  (let [input '(,".U."
+                 "LTR"
+                 ".B.") ]
+    (is (= \U (next-place input 'UP)))
+    (is (= \B (next-place input 'BOTTOM)))
+    (is (= \L (next-place input 'LEFT)))
+    (is (= \R (next-place input 'RIGHT)))))
+
+(deftest test-get-place-in-lab
+  (let [lab '(,".U."
+               "LTR"
+               ".B.") ]
+    (is (= \.  (get-place-in-lab lab '(0 0))))
+    (is (= \U  (get-place-in-lab lab '(0 1))))
+    (is (= \B  (get-place-in-lab lab '(2 1))))
+    (is (= nil (get-place-in-lab lab '(20 10))))
+    ))
 
 (deftest test-next-move
-  (testing "go right"
-    (let [input '("T.")]
-      ; (is (= 'RIGHT (next-move input 'RIGHT)))
-      )
-    )
-  )
-
+  (let [input '("T.")]
+    (is (= 'RIGHT (next-move input 'RIGHT)))
+    (is (= 'FAIL (next-move input 'FAIL)))
+    (is (= 'FAIL (next-move input 'LEFT)))
+    (is (= 'FAIL (next-move input 'UP)))
+    (is (= 'FAIL (next-move input 'BOTTOM))))
+  (let [input '( "..."
+                 ".T."
+                 "...")]
+    (is (= 'UP     (next-move input 'UP)))
+    (is (= 'BOTTOM (next-move input 'BOTTOM)))
+    (is (= 'LEFT   (next-move input 'LEFT)))
+    (is (= 'RIGHT  (next-move input 'RIGHT)))))
