78a5613d003c844b27cf92886c7c8fbef387b005
Hettomei <itsumo.sibyllin@gmail.com>
2017-10-03 21:50:32 +0200

    tel seems work

--- javascript/tel.js
+++ javascript/tel.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-console
 const l = console.log;
-const pt = (ll, i = 0) => ll.forEach((e) => {
+const pt = ll => i => ll.forEach((e) => {
   l(Array(i).fill(' ').join('') + e.node);
-  pt(e.children, i + 1);
+  pt(e.children)(i + 1);
 });
 
 // var N = parseInt(readline());
@@ -25,19 +25,15 @@ const place = tree => ([number, ...tel]) => {
 
 // for (var i = 0; i < N; i++) {
   // const tel = readline().split('');
-const tel1 = '01234'.split('');
+const tel1 = '01234567'.split('');
 const tel2 = '0123345'.split('');
 const tel3 = '0223345'.split('');
 place(all)(tel1);
 place(all)(tel2);
 place(all)(tel3);
 
-function count(arr = []) {
-  if (arr.length) {
-    return arr.length + reduce((acc,node) => acc + count(arr.children));
-  }
+pt(all)(0);
+const flatten = arr => arr.reduce((a, b) => a.concat(b.node).concat(flatten(b.children)), []);
 
-  return 0;
-}
-
-l(count(all));
+l(flatten(all).join());
+l(flatten(all).length);
