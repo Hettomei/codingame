f46a055b61fea777a7155359bdf047496efaa97c
Timothée Gauthier <itsumo.sibyllin@gmail.com>
2017-11-17 22:01:16 +0100

    start mad max

---
+++ mad-max/index.js
@@ -0,0 +1,30 @@
+while (true) {
+    var myScore = parseInt(readline());
+    var enemyScore1 = parseInt(readline());
+    var enemyScore2 = parseInt(readline());
+    var myRage = parseInt(readline());
+    var enemyRage1 = parseInt(readline());
+    var enemyRage2 = parseInt(readline());
+    var unitCount = parseInt(readline());
+    for (var i = 0; i < unitCount; i++) {
+        var inputs = readline().split(' ');
+        var unitId = parseInt(inputs[0]);
+        var unitType = parseInt(inputs[1]);
+        var player = parseInt(inputs[2]);
+        var mass = parseFloat(inputs[3]);
+        var radius = parseInt(inputs[4]);
+        var x = parseInt(inputs[5]);
+        var y = parseInt(inputs[6]);
+        var vx = parseInt(inputs[7]);
+        var vy = parseInt(inputs[8]);
+        var extra = parseInt(inputs[9]);
+        var extra2 = parseInt(inputs[10]);
+    }
+
+    // Write an action using print()
+    // To debug: printErr('Debug messages...');
+
+    print('WAIT');
+    print('WAIT');
+    print('WAIT');
+}
