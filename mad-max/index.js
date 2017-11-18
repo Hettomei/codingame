beea679c2ae869611b82e11c4a65f23fb7895ebd
Timothée Gauthier <itsumo.sibyllin@gmail.com>
2017-11-19 00:01:47 +0100

    works leaguge bois 2

--- mad-max/index.js
+++ mad-max/index.js
@@ -1,20 +1,23 @@
 /* global readline printErr print */
 /* eslint-disable no-restricted-globals */
-/* eslint-disable no-unused-vars */
 
 // Write an action using print()
 const p = printErr;
 const TRUE = true;
-const readSplit = _ => readline().split(' ').map(j => parseInt(j, 10));
+const readSplit = () => readline().split(' ').map(j => parseInt(j, 10));
 const pp = a => p(JSON.stringify(a, null, 1));
 
-const REAPER = 0;
+// const REAPER = 0;
+// const DESTROYER = 1; // kill les tankers
+const TANKER = 3; // Contient de l'eau
 const EPAVE = 4;
+
+const f = n => b => b.type === n;
 
 function toObj(array) {
   const [
-    unitId,
-    unitType, // 0 (Reaper), 4 (Épave)
+    id,
+    type, // 0 (Reaper), 4 (Épave)
     player, // id du joueur pour les Reapers, -1 sinon
     mass, // Masse du véhicule, -1 pour les Épaves
     radius, x, y,
@@ -24,7 +27,7 @@ function toObj(array) {
   ] = array;
 
   return {
-    unitId, unitType, player, mass, radius, x, y, vx, vy, water, extra2,
+    id, type, player, mass, radius, x, y, vx, vy, water, extra2,
   };
 }
 
@@ -39,7 +42,7 @@ function distance(a, b) {
 // 0 <= acc <= 300
 function acc(me, b) {
   const d = distance(me, b);
-  const v = distance({ x: 0, y: 0 }, { x: me.vx, y: me.vy });
+  // const v = distance({ x: 0, y: 0 }, { x: me.vx, y: me.vy });
   if (d > 800) {
     return 300;
   } else if (d > 700) {
@@ -52,14 +55,24 @@ function acc(me, b) {
   return 0;
 }
 
+function toClosest(me, others) {
+  const closestOthers = others.slice().sort((a, b) => distance(me, a) - distance(me, b));
+  const best = closestOthers[0];
+  return `${best.x} ${best.y} ${acc(me, best)}`;
+}
+
+function follow(me, other) {
+  return `${other.x} ${other.y} ${acc(me, other)}`;
+}
+
 while (TRUE) {
   const myScore = parseInt(readline(), 10);
   const enemyScore1 = parseInt(readline(), 10);
   const enemyScore2 = parseInt(readline(), 10);
-
   const myRage = parseInt(readline(), 10);
   const enemyRage1 = parseInt(readline(), 10);
   const enemyRage2 = parseInt(readline(), 10);
+  p(myScore, enemyScore1, enemyScore2, myRage, enemyRage1, enemyRage2);
 
   const unitCount = parseInt(readline(), 10);
   const all = new Array(unitCount)
@@ -67,16 +80,25 @@ while (TRUE) {
     .map(readSplit)
     .map(toObj);
 
-  all.forEach(m => p(...m));
-  const me = all[0];
-  // pp(me);
+  const reaper = all[0];
+  const destroyer = all[1];
+  // pp(reaper);
+  // pp(destroyer);
 
-  const repears = all.slice(1).filter(b => b.unitType === REAPER);
-  const epaves = all.filter(b => b.unitType === EPAVE);
+  const epaves = all.filter(f(EPAVE));
+  if (epaves.length) {
+    print(toClosest(reaper, epaves));
+  } else {
+    p('follow');
+    print(follow(reaper, destroyer));
+  }
 
-  const closestEpaves = epaves.slice().sort((a, b) => distance(me, a) - distance(me, b));
-  const best = closestEpaves[0];
-  print(`${best.x} ${best.y} ${acc(me, best)}`);
-  print('WAIT');
+  const tankers = all.filter(f(TANKER));
+  if (tankers.length) {
+    print(toClosest(destroyer, tankers));
+  } else {
+    print('WAIT');
+  }
+
   print('WAIT');
 }
