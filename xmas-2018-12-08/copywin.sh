546f643ed0815c2b7b02e591f04b21a41aab75c0
Timothée Gauthier <t.gauthier42@gmail.com>
2018-12-11 00:53:07 +0100

    xmas: add how to

--- xmas-2018-12-08/copywin.sh
+++ xmas-2018-12-08/copywin.sh
@@ -1,2 +1,10 @@
-cp -v -u -R /home/tgauthier/programmes/codingame/xmas-2018-12-08 /mnt/c/Users/tim/programmes/
+set -e
+
+CLJ_PATH=/home/tgauthier/programmes/codingame/xmas-2018-12-08
+WIN_PATH=/mnt/c/Users/tim/programmes/
+
+echo "(ns Player" > $CLJ_PATH/windows_build.clj
+tail -n +2 "$CLJ_PATH/src/xmas/core.clj" >> $CLJ_PATH/windows_build.clj
+
+cp -v -u -R $CLJ_PATH $WIN_PATH
 echo 'ok'
--- xmas-2018-12-08/readme.md
+++ xmas-2018-12-08/readme.md
@@ -3,3 +3,7 @@ if on windows, please run in  bash :
 ```
 find . | entr sh -c './copywin.sh'
 ```
+
+to run :
+
+lein run < turn/turn1.txt
