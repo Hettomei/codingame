68f1d8a03dedc44505b5f9a274b3a901ccb3458c
Timothée Gauthier <t.gauthier42@gmail.com>
2018-12-13 23:17:45 +0100

    xmas: not working read-line

--- xmas-2018-12-08/src/xmas/core.clj
+++ xmas-2018-12-08/src/xmas/core.clj
@@ -9,18 +9,19 @@
 ; (doseq [i arg] (pprint i))
 ; (println "-----------")))
 
-(defn ppprint [& arg]
-  (doseq [i arg] (pprint i))
-  (println "-----------"))
+; (defn ppprint [& arg]
+;   (doseq [i arg] (pprint i))
+  ; (println "-----------"))
 
+; doall is a workaround to ensure repeatedly + read-line have done all work
 (defn build-area []
-  (into [] (repeatedly 7 (fn [] (split (read-line) #" ")))))
+  (let [a (repeatedly 7 #(split (read-line) #" "))]
+    (dorun 7 a)
+    a))
 
 ;numPlayerCards, playerX, playerY, playerTile:
-(defn fetch-player []
-  (let [b (read-line)
-        a (split b #" ")
-        [numPlayerCards x y playerTile] a]
+(defn fetch-player [line]
+  (let [[numPlayerCards x y playerTile] (split line #" ")]
     {
      :restingQuests (Integer/parseInt numPlayerCards)
      :playerTile playerTile
@@ -28,10 +29,8 @@
      :y (Integer/parseInt y)
      }))
 
-(defn build-item []
-  (let [b (read-line)
-        a (split b #" ")
-        [itemName x y playerId] a]
+(defn build-item [line]
+  (let [[itemName x y playerId] (split line #" ")]
     {
      :itemName itemName
      :playerId (Integer/parseInt playerId)
@@ -39,46 +38,50 @@
      :y (Integer/parseInt y)
      }))
 
+; doall is a workaround to ensure repeatedly + read-line have done all work
 (defn build-items [number]
-  (repeatedly number build-item))
+  (let [a (repeatedly number #(build-item (read-line)))]
+    (doall a)
+    a))
 
-(defn build-quest []
-  (let [b (read-line)
-        a (split b #" ")
-        [itemName playerId] a]
+(defn build-quest [line]
+  (let [[itemName playerId] (split line #" ")]
     {
      :itemName itemName
-     :playerId playerId
+     :playerId (Integer/parseInt playerId)
      }))
 
+; doall is a workaround to ensure repeatedly + read-line have done all work
 (defn build-quests [number]
-  (ppprint number)
-  (repeatedly number build-quest))
+  (let [a (repeatedly number #(build-quest (read-line)))]
+    (doall a)
+    a))
 
 (defn push []
-  (pprint "PUSH")
+  ; (ppprint "PUSH")
   (let [area (build-area)
-        player1 (fetch-player)
-        player2 (fetch-player)
+        player1 (fetch-player (read-line))
+        player2 (fetch-player (read-line))
         items (build-items (Integer/parseInt (read-line)))
-        ; restingQuests (build-quests (Integer/parseInt (read-line)))
-        ]
+        resting-quests (build-quests (Integer/parseInt (read-line)))]
 
-    (ppprint area)
-    (ppprint "player1" player1)
-    (ppprint "player2" player2)
-    (ppprint "items" items)
-    (ppprint "items a" (Integer/parseInt (read-line)))
-    (doseq [x [1 2]] (ppprint (read-line)))
-    ; (ppprint "restingQuests" restingQuests)
+    ; (ppprint area)
+    ; (ppprint "player1" player1)
+    ; (ppprint "player2" player2)
+    ; (ppprint "items" items)
+    ; (ppprint "resting-quests" resting-quests)
+  "PUSH 0 UP"
     )
-  (System/exit 0))
+    )
 
 (defn move []
   "MOVE"
-  "PUSH 0 UP"
-  (System/exit 0))
+  "PUSH 0 UP")
 
 (defn -main [& args]
-  (while true
-    (println (if (= "0" (read-line)) (push) (move)))))
+  (let [all (repeatedly 16 read-line)]
+    (doall all)
+    (let [area (build-area all)])
+  )))
+  ; (while true
+  ;   (println (if (= "1" (read-line)) (push) (move)))))
