12369f01677d7308fdaa5dae3ec7ce44532d5cee
Hettomei <t.gauthier42@gmail.com>
2019-03-19 14:14:55 +0100

    ascii: Add first test

--- javascript/asciiart/ascii.js
+++ javascript/asciiart/ascii.js
@@ -2,7 +2,12 @@ function toto() {
   return 'toto';
 }
 
+function extract(str, index, width) {
+  return str.slice(index * width, (index * width) + width);
+}
+
 
 module.exports = {
   toto,
+  extract,
 };
--- javascript/asciiart/test/ascii.spec.js
+++ javascript/asciiart/test/ascii.spec.js
@@ -19,4 +19,34 @@ describe('Array', () => {
       expect(ascii.toto()).to.equal('toto');
     });
   });
+
+  describe('.extract', () => {
+    it('découpe sur l index', () => {
+      const str = '123456789';
+      const index = 0;
+      const width = 4;
+      expect(ascii.extract(str, index, width)).to.equal('1234');
+    });
+
+    it('découpe sur l index', () => {
+      const str = '123-234-345-456-567';
+      const index = 2;
+      const width = 4;
+      expect(ascii.extract(str, index, width)).to.equal('345-');
+    });
+
+    it('cas reel : decoupe', () => {
+      const str = ' #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ';
+      const index = 5;
+      const width = 4;
+      expect(ascii.extract(str, index, width)).to.equal('### ');
+    });
+
+    it('découpe sur l index', () => {
+      const str = '12345-67890-abcdef';
+      const index = 1;
+      const width = 6;
+      expect(ascii.extract(str, index, width)).to.equal('67890-');
+    });
+  });
 });
