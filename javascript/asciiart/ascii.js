9f6e05d7b54bd7ed8f75038845f5d7ef65a32843
Hettomei <t.gauthier42@gmail.com>
2019-03-19 14:54:19 +0100

    ascii : update extract to work in array

--- javascript/asciiart/ascii.js
+++ javascript/asciiart/ascii.js
@@ -3,14 +3,27 @@ function toto() {
 }
 
 function extract(str, index, width) {
-  return str.slice(
-    index * width,
-    (index * width) + width,
-  );
+  let output = '';
+
+  index.forEach((i) => {
+    output += str.slice(
+      i * width,
+      (i * width) + width,
+    );
+  });
+
+  return output;
 }
 
+function extractElegant(str, index, width) {
+  return index.reduce((acc, i) => acc + str.slice(
+    i * width,
+    (i * width) + width,
+  ), '');
+}
 
 module.exports = {
   toto,
   extract,
+  extractElegant,
 };
