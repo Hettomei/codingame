6a4760fe7abbe2e2412557900cf5c703b9a9866d
Hettomei <t.gauthier42@gmail.com>
2019-03-19 11:09:58 +0100

    ascii: sol1, step 1

---
+++ javascript/asciiart/solution1.js
@@ -0,0 +1,34 @@
+// eslint-disable no-console
+
+const fs = require('fs');
+
+const input = fs.readFileSync('./level1.txt', 'utf8');
+// const input = fs.readFileSync('./level2.txt', 'utf8')
+// const input = fs.readFileSync('./level3.txt', 'utf8')
+// const input = fs.readFileSync('./level4.txt', 'utf8')
+// const input = fs.readFileSync('./level5.txt', 'utf8')
+
+const a = input.split('\n');
+const width = Number(a[0]);
+const height = Number(a[1]);
+const mixedLetters = a.slice(3, height + 3);
+
+const letters = [];
+
+mixedLetters.forEach((line) => {
+  let j = 0;
+  for (let i = 0; i < line.length; i += width) {
+    if (!letters[j]) {
+      letters[j] = '';
+    }
+
+    if (letters[j]) {
+      letters[j] += '\n';
+    }
+
+    letters[j] += `${line.slice(i, i + width)}`;
+    j += 1;
+  }
+});
+
+letters.forEach(l => console.log(l));
