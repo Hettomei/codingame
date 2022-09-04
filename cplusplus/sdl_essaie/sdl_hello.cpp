7ed3dd0362f6ba7732461e3766fa239a797a8404
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2022-09-04 22:51:29 +0200

    SDL add logs

--- cplusplus/sdl_essaie/sdl_hello.cpp
+++ cplusplus/sdl_essaie/sdl_hello.cpp
@@ -5,8 +5,10 @@ const int WIDTH = 1100, HEIGHT = 900;
 
 int main(int argc, char* args[]) {
   std::cout << "Hello World!";
+
   if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-    return 1;
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > %s", SDL_GetError());
+    return EXIT_FAILURE;
   }
 
   SDL_Window* window = SDL_CreateWindow("GAME",
@@ -16,7 +18,9 @@ int main(int argc, char* args[]) {
                                         SDL_WINDOW_ALLOW_HIGHDPI);
 
   if(window == NULL) {
-    return 1;
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[DEBUG] > window == null %s", SDL_GetError());
+    SDL_Quit();
+    return EXIT_FAILURE;
   }
   
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
@@ -24,6 +28,20 @@ int main(int argc, char* args[]) {
   SDL_RenderClear(renderer);
 
   SDL_RenderPresent(renderer);
+
+  SDL_RendererInfo infoRenderer;
+  SDL_GetRendererInfo(renderer, &infoRenderer);
+
+  if (infoRenderer.flags & SDL_RENDERER_ACCELERATED) {
+    SDL_Log("Le rendu est gérer par SDL_RENDERER_ACCELERATED.");
+  }
+  if (infoRenderer.flags & SDL_RENDERER_SOFTWARE) {
+    SDL_Log("Le rendu est gérer par SDL_RENDERER_SOFTWARE.");
+  }
+
+  if (infoRenderer.flags & SDL_RENDERER_TARGETTEXTURE) {
+    SDL_Log("Le rendu est autorisé sur des textures.");
+  }
   
   SDL_Event event;
   while(1) {
@@ -34,8 +52,9 @@ int main(int argc, char* args[]) {
     }
   }
 
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
 
   SDL_Quit();
-  return 0;
+  return EXIT_SUCCESS;
 }
