ff3040ab194ee2e6fbe18b9ce02346bed933a7fe
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2023-07-09 21:11:57 +0200

    Update install

--- cplusplus/install-for-mingw.sh
+++ cplusplus/install-for-mingw.sh
@@ -4,8 +4,11 @@ echo "If it automatically close, restart this script"
 sleep 3
 
 pacman -Syu
-pacman -S base-devel gcc vim cmake git
-pacman -S --needed mingw-w64-x86_64-toolchain
-pacman -S mingw64/mingw-w64-x86_64-cmake
-pacman -S clang # to have clang-format
+pacman -S --noconfirm base-devel gcc vim cmake git
+pacman -S --noconfirm --needed mingw-w64-ucrt-x86_64-toolchain
+pacman -S --noconfirm mingw64/mingw-w64-x86_64-cmake
+# pacman -S --noconfirm mingw64/mingw-w64-ucrt-x86_64-cmake # inexistant
+pacman -S --noconfirm clang # to have clang-format
+
+echo "Test de G++ :"
 g++  --version
