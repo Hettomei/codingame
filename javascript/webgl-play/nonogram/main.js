45fb60efbb421f01b13aa0b64743198bddd6da9c
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2023-10-24 21:02:02 +0000

    nonogram: can render a 300x200 grid, this way

--- javascript/webgl-play/nonogram/main.js
+++ javascript/webgl-play/nonogram/main.js
@@ -14,20 +14,19 @@ function animate() {
   stats.update();
 }
 
-function create_cube(size) {
-  const geometry = new THREE.BoxGeometry(size, size, size);
-  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-  return new THREE.Mesh(geometry, material);
-}
-
 function build_nonograme(scene, number_cases_x, number_cases_y) {
   const cube_size = 10;
   const espace = 2;
   const init_x = (number_cases_x * cube_size + espace * number_cases_x) / 2;
   const init_y = (number_cases_y * cube_size + espace * number_cases_y) / 2;
+
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const geometry = new THREE.BoxGeometry(cube_size, cube_size, cube_size);
+  let cube;
+
   for (let j = 0; j < number_cases_y; j++) {
     for (let i = 0; i < number_cases_x; i++) {
-      const cube = create_cube(cube_size);
+      cube = new THREE.Mesh(geometry, material);
       cube.position.x = i * cube_size + espace * i - init_x;
       cube.position.y = j * cube_size + espace * j - init_y;
       scene.add(cube);
