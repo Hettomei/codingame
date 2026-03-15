5b70331a9c140c67fef7ccb8515fe693ae9c174b
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2026-03-15 16:34:53 +0100

    display size

--- java/compet/run.sh
+++ java/compet/run.sh
@@ -1,4 +1,8 @@
 set -eu
 
+echo "##########################################################"
+ls -alh src/main/java/com/timit/compet/Player.java | awk '{print "##  " $5 "   " $9 "   ##"}'
+echo "##########################################################"
+
 mvn spotless:apply package
 java -jar target/tim-challenge-1.0-SNAPSHOT.jar < "seeds/seed_-974398609313948000.txt"
